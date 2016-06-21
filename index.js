57f25f936dc921185be25b6837a0f6430ccbdba7
Asim Hussain <jawache@gmail.com>
2016-06-21 22:49:39 +0100

    Added S3Adaptor

--- index.js
+++ index.js
@@ -3,43 +3,50 @@
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
+var S3Adapter = require('parse-server').S3Adapter;
 var path = require('path');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) {
-  console.log('DATABASE_URI not specified, falling back to localhost.');
+	console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  },
-  // Enable email verification
-  verifyUserEmails: true,
-  // The public URL of your app.
-  // This will appear in the link that is used to verify email addresses and reset passwords.
-  // Set the mount path as it is in serverURL
-  publicServerURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
-  // Your apps name. This will appear in the subject and body of the emails that are sent.
-  appName: process.env.APP_NAME,
-  // The email adapter
-  emailAdapter: {
-    module: 'parse-server-simple-mailgun-adapter',
-    options: {
-      // The address that your emails come from
-      fromAddress: process.env.EMAIL_FROM,
-      // Your domain from mailgun.com
-      domain: process.env.MAILGUN_DOMAIN,
-      // Your API key from mailgun.com
-      apiKey: process.env.MAILGUN_API_KEY
-    }
-  }
+	databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+	cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+	appId: process.env.APP_ID || 'myAppId',
+	masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+	serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+	liveQuery: {
+		classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+	},
+	// Enable email verification
+	verifyUserEmails: true,
+	// The public URL of your app.
+	// This will appear in the link that is used to verify email addresses and reset passwords.
+	// Set the mount path as it is in serverURL
+	publicServerURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
+	// Your apps name. This will appear in the subject and body of the emails that are sent.
+	appName: process.env.APP_NAME,
+	// The email adapter
+	emailAdapter: {
+		module: 'parse-server-simple-mailgun-adapter',
+		options: {
+			// The address that your emails come from
+			fromAddress: process.env.EMAIL_FROM,
+			// Your domain from mailgun.com
+			domain: process.env.MAILGUN_DOMAIN,
+			// Your API key from mailgun.com
+			apiKey: process.env.MAILGUN_API_KEY
+		}
+	},
+	filesAdapter: new S3Adapter(
+		"S3_ACCESS_KEY",
+		"S3_SECRET_KEY",
+		"S3_BUCKET",
+		{directAccess: true}
+	)
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -55,20 +62,20 @@ var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
-app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+app.get('/', function (req, res) {
+	res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
 });
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
+app.get('/test', function (req, res) {
+	res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
-httpServer.listen(port, function() {
-    console.log('parse-server-example running on port ' + port + '.');
+httpServer.listen(port, function () {
+	console.log('parse-server-example running on port ' + port + '.');
 });
 
 // This will enable the Live Query real-time server
