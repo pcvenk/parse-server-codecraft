91f6b74fb76c752cd83c1c68bceb5377fcd1486d
Asim Hussain <jawache@gmail.com>
2016-07-18 17:58:09 +0100

    Uncommented lines to match end of course

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
-// var S3Adapter = require('parse-server').S3Adapter;
+ var S3Adapter = require('parse-server').S3Adapter;
 var path = require('path');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
@@ -17,40 +17,40 @@ var api = new ParseServer({
 	serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
 	
 	//**** Security Settings ****//
-	// allowClientClassCreation: process.env.CLIENT_CLASS_CREATION || false, 
+	 allowClientClassCreation: process.env.CLIENT_CLASS_CREATION || false,
 	appId: process.env.APP_ID || 'myAppId',
 	masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!	
 	
 	//**** Live Query ****//
-	// liveQuery: {
-	// 	classNames: ["TestObject", "Place", "Team", "Player", "ChatMessage"] // List of classes to support for query subscriptions
-	// },
+	 liveQuery: {
+	 	classNames: ["TestObject", "Place", "Team", "Player", "ChatMessage"] // List of classes to support for query subscriptions
+	 },
 
 	//**** Email Verification ****//
 	/* Enable email verification */
-	// verifyUserEmails: true,
+	 verifyUserEmails: true,
 	/* The public URL of your app */
 	// This will appear in the link that is used to verify email addresses and reset passwords.
 	/* Set the mount path as it is in serverURL */
-	// publicServerURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
+	 publicServerURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
 	/* This will appear in the subject and body of the emails that are sent */
-	// appName: process.env.APP_NAME || "CodeCraft", 
+	 appName: process.env.APP_NAME || "CodeCraft",
 
-	// emailAdapter: {
-	// 	module: 'parse-server-simple-mailgun-adapter',
-	// 	options: {
-	// 		fromAddress: process.env.EMAIL_FROM || "test@example.com",
-	// 		domain: process.env.MAILGUN_DOMAIN || "example.com",
-	// 		apiKey: process.env.MAILGUN_API_KEY  || "apikey"
-	// 	}
-	// },
+	 emailAdapter: {
+	 	module: 'parse-server-simple-mailgun-adapter',
+	 	options: {
+	 		fromAddress: process.env.EMAIL_FROM || "test@example.com",
+	 		domain: process.env.MAILGUN_DOMAIN || "example.com",
+	 		apiKey: process.env.MAILGUN_API_KEY  || "apikey"
+	 	}
+	 },
 	
 	//**** File Storage ****//
-	// filesAdapter: new S3Adapter(
-	// 	{
-	// 		directAccess: true
-	// 	}
-	// )
+	 filesAdapter: new S3Adapter(
+	 	{
+	 		directAccess: true
+	 	}
+	 )
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
