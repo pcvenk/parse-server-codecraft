69d8813d8df68404309884e0c05a6eea19d1334e
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2016-10-20 17:43:12 +0200

    Added various queries to search and retrive data from the DB

---
+++ exercise/queries.js
@@ -0,0 +1,63 @@
+var Player = Parse.Object.extend('Player');
+// var Team = Parse.Object.extend('Team');
+
+var q = new Parse.Query('Player');
+q.limit(1000);
+q.descending('dateOfBirth');
+q.first()
+    .then(function(player){
+        console.log('The youngest player is ' + player.get('name'));
+    });
+
+
+var q = new Parse.Query('Player');
+q.limit(1000);
+q.descending('jerseyNumber');
+q.first()
+    .then(function(player){
+        console.log('Player with the highest jersey number is ' +
+        player.get('jerseyNumber') + ' and his name is ' + player.get('name'));
+    });
+
+var q = new Parse.Query('Player');
+q.equalTo('position', 'Keeper');
+q.equalTo('nationality', 'Wales');
+q.find()
+    .then(function(players) {
+        console.log('There are ' + players.length + ' Welsh keepers');
+});
+
+var q = new Parse.Query('Player');
+q.descending('marketValue');
+q.skip(10);
+q.limit(3);
+q.find()
+    .then(function(players){
+        console.log('The 11th, 12th and 13th most expensive players are:');
+        for(var i = 0;i < players.length; i++) {
+            console.log(players[i].get('name') + ' who cost ' + players[i].get('marketValue'));
+        }
+    });
+
+var q = new Parse.Query("Player");
+q.notContainedIn("teamCode", ["MCFC", "SCFC", "LFC"]);
+q.count()
+    .then(function(count){
+        console.log(count + " players are not playing for MCFC, SCFC or LFC");
+    });
+
+var q1 = new Parse.Query('Player');
+q1.lessThan('jerseyNumber', 10);
+
+var q2 = new Parse.Query('Player');
+q2.greaterThan('jerseyNumber', 50);
+
+
+var compoudQuery = Parse.Query.or(q1, q2);
+compoudQuery.find()
+    .then(function(players){
+        console.log('Players whose number is less than 10 or more than 50 are:');
+        for(var i = 0; i < players.length; i++){
+            console.log(players[i].get('name'));
+        }
+    });
--- exercise/run.js
+++ exercise/run.js
@@ -4,78 +4,158 @@ Parse.initialize("fo234155135dgwefb");
 Parse.serverURL = "https://parse-server-exmpl.herokuapp.com/parse";
 
 
-var Customer = Parse.Object.extend("Customer");
-var customer = new Customer();
+// var Customer = Parse.Object.extend("Customer");
+// var customer = new Customer();
+//
+// var data = {
+//
+//     'name': 'Primoz',
+//     'surname': 'Cvenkelj',
+//     'profession': 'js guru'
+//
+// };
+//
+// customer.save(data, {
+//
+//     success: function(user){
+//
+//         console.log("A new user with the id " + user.id + " was saved!");
+//
+//         var q = new Parse.Query("Customer");
+//         q.get(user.id, {
+//
+//             success: function(user) {
+//
+//                 console.log("A user with the id " + user.id + " was retrieved!");
+//
+//                 user.set('name', 'Cecko');
+//                 user.set('age', 30);
+//
+//                 user.save(null, {
+//
+//                     success: function(user) {
+//
+//                         console.log("A user with the id " + user.id + " was updated!");
+//
+//                         user.destroy({
+//
+//                             success: function(user) {
+//
+//                                 console.log('User ' +user.id +'was deleted');
+//
+//                             },
+//                             error: function(err) {
+//
+//
+//                             }
+//                         });
+//
+//                     },
+//
+//                     error: function(user, err) {
+//
+//                         console.log(err);
+//
+//                     }
+//
+//                 });
+//
+//             },
+//
+//             error: function(user, err) {
+//
+//                 console.log(err);
+//             }
+//
+//         });
+//
+//     },
+//     error: function(user, err){
+//
+//         console.log(err);
+//
+//     }
+//
+// });
 
-var data = {
+/*var Order = Parse.Object.extend('Order');
+var order = new Order();
 
-    'name': 'Primoz',
-    'surname': 'Cvenkelj',
-    'profession': 'js guru'
+var orderData = {
+
+    order_id: 'OI_1234',
+    amount: 112.3,
+    date: Date.now()
 
 };
 
-customer.save(data, {
-
-    success: function(user){
-
-        console.log("A new user with the id " + user.id + " was saved!");
-
-        var q = new Parse.Query("Customer");
-        q.get(user.id, {
-
-            success: function(user) {
-
-                console.log("A user with the id " + user.id + " was retrieved!");
-
-                user.set('name', 'Cecko');
-                user.set('age', 30);
-
-                user.save(null, {
-
-                    success: function(user) {
-
-                        console.log("A user with the id " + user.id + " was updated!");
-
-                        user.destroy({
-
-                            success: function(user) {
-
-                                console.log('User ' +user.id +'was deleted');
-
-                            },
-                            error: function(err) {
+order.save(orderData)
+    .then(function success(obj){
+        console.log('You created the order' + obj.id+ '!');
+    }, function error(obj, err){
+        console.log(err);
+});
 
 
-                            }
-                        });
+var Item = Parse.Object.extend('Item');
+var items = [
 
-                    },
-
-                    error: function(user, err) {
-
-                        console.log(err);
-
-                    }
-
-                });
-
-            },
-
-            error: function(user, err) {
-
-                console.log(err);
-            }
-
-        });
+    {
+        item_id: 1,
+        name: 'hat',
+        cost: 12.3,
+        order: order
 
     },
-    error: function(user, err){
+    {
+        item_id: 2,
+        name: 'shoes',
+        cost: 50,
+        order: order
 
-        console.log(err);
+    },
+    {
+        item_id: 3,
+        name: 'glove',
+        cost: 50,
+        order: order
 
     }
 
-});
+
+];
+
+for(var i = 0; i < items.length; i++) {
+    var item = new Item();
+    item.save(items[i])
+        .then(function success(item) {
+            console.log('Item created with id' + item.id);
+        }, function error(item, err) {
+            console.log(err);
+        });
+}*/
+
+
+function onComplete(){
+
+    var promise = new Parse.Promise();
+
+    setTimeout(function(){
+
+        console.log('Timeout set');
+        promise.resolve('Promise resolved');
+
+    }, 2000);
+
+    return promise;
+};
+
+onComplete()
+    .then(function success(message){
+
+        console.log(message);
+        console.log('Now its time to go to work');
+
+    });
 
 
