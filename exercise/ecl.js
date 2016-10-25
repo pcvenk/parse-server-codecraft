4d0199ba844ca488fa55818afb67dee9074182bc
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2016-10-25 17:40:26 +0200

    Added the admin role for a selected user and assigned that role to a ceratin object with right and/or write access. The public has write-only access

--- exercise/ecl.js
+++ exercise/ecl.js
@@ -5,19 +5,42 @@ Parse.serverURL = "https://parse-server-exmpl.herokuapp.com/parse";
 
 var TestObject = Parse.Object.extend('TestObject');
 
+function createRoles(){
+    // creating a new ACL needed for the Admin role
+    var roleACL = new Parse.ACL();
+    roleACL.setPublicReadAccess(true);
+    roleACL.setWriteAccess(Parse.User.current(), true);
+
+    //creating a new role, assigning it the role ACL; assign the user to that role and save it
+    var role = new Parse.Role('Admin', roleACL);
+    role.save()
+        .then(function() {
+            role.getUsers().add(Parse.User.current());
+            role.save()
+                .then(function(role) {
+                   console.log('Role created '+role.id);
+                });
+        });
+}
+
+Parse.User.logIn('Primoz', 'mypassword')
+    .then(createRoles);
+
+
 function main(){
   console.log('Main called');
 
   var user = Parse.User.current();
   //  creating a new instance of the Parse.ACL method
   var groupACL = new Parse.ACL();
-  //  giving a specific user read and/or write access
-  groupACL.setWriteAccess(user, true);
-  groupACL.setReadAccess(user, false);
+  //  giving a specific role read and/or write access
+  groupACL.setRoleWriteAccess("Admin", true);
+  groupACL.setRoleReadAccess("Admin", false);
   //  giving the public read-only access
   groupACL.setPublicReadAccess(true);
 
   var testObject = new TestObject();
+  //  setting the ACL to the testObject
   testObject.setACL(groupACL);
 
   //  setting ACL read and write rights to the current user
