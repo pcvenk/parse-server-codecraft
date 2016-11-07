992ac7aff6202118447d3109e2c2bd52c3c350c0
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2016-11-07 16:13:07 +0100

    Added a query parameter to search for places within a certain range from my location.

--- exercise/geoloaction.js
+++ exercise/geoloaction.js
@@ -6,11 +6,27 @@ var Place = Parse.Object.extend('Place');
 //creating a new geoPoint and pass in the user location
 var myLocation = new Parse.GeoPoint({latitude: 51.54444, longitude: -0.022974});
 
+// var q = new Parse.Query('Place');
+// //adding a query parameter to search only coffeshops near my location and limiting the result
+// //to 25 hits
+// q.near('geo', myLocation);
+// q.limit(25);
+// q.find().then(function(coffeeshops){
+//
+//     for(var i=0; i<coffeeshops.length; i++){
+//         var coffeeshop = coffeeshops[i];
+//         //printing put the distance in kilometers to coffeeshops
+//         var distance = coffeeshop.get('geo').kilometersTo(myLocation);
+//         //find all the places around me, and display their name
+//         console.log(coffeeshop.get('name')+ "," + " distance " + distance + " kilometers");
+//     }
+//
+// });
+
 var q = new Parse.Query('Place');
-//adding a query parameter to search only coffeshops near my location and limiting the result
-//to 25 hits
-q.near('geo', myLocation);
-q.limit(25);
+//adding a query parameter to search only coffeshops within 5 kilometers and limit the result to
+//10 cofeeshops
+q.withinKilometers('geo', myLocation, 5);
 q.find().then(function(coffeeshops){
 
     for(var i=0; i<coffeeshops.length; i++){
