8cd31d5a15c605b27aa7a6f88d5a172f6e8ee232
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2016-11-21 11:08:49 +0100

    Added client-side code for the player's notes extra functionality

--- cloud/functions/resetPlayerNotes.js
+++ cloud/functions/resetPlayerNotes.js
@@ -1,15 +1,18 @@
 Parse.Cloud.define("resetPlayerNotes", function(request, response) {
 	// Set up to modify user data
 	Parse.Cloud.useMasterKey();
+	//define a new query
 	var q = new Parse.Query("Player");
+	//run the query on each player
 	q.each(function (player) {
 		// Update to plain value passed in
 		player.set("notes", "");
+		//save the player
 		return player.save();
-	}).then(function () {
+	}).then(function() {
 		// Set the job's success status
 		response.success("Player notes reset completed successfully.");
-	}, function () {
+	}, function() {
 		// Set the job's error status
 		response.error("Uh oh, something went wrong.");
 	});
--- exercise/cloud.js
+++ exercise/cloud.js
@@ -2,18 +2,23 @@
 Parse.initialize("fo234155135dgwefb");
 Parse.serverURL = "https://parse-server-exmpl.herokuapp.com/parse";
 
-Parse.Cloud.run('hello')
+// Parse.Cloud.run('hello')
+//     .then(function(res){
+//        console.log(res);
+//     });
+
+
+// var TestObject = Parse.Object.extend('TestObject');
+// var testObject = new TestObject();
+// testObject.set('foo', 'Value defined');
+// testObject.save()
+//     .then(function(){
+//        console.log('Saved');
+//     }, function(err){
+//         console.log(err);
+//     });
+
+Parse.Cloud.run('resetPlayerNotes')
     .then(function(res){
-       console.log(res);
-    });
-
-
-var TestObject = Parse.Object.extend('TestObject');
-var testObject = new TestObject();
-testObject.set('foo', 'Value defined');
-testObject.save()
-    .then(function(){
-       console.log('Saved');
-    }, function(err){
-        console.log(err);
+        console.log(res);
     });
