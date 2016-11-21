9563353790031f7c24942926a6e1043b782b37d0
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2016-11-21 10:33:08 +0100

    Added comments for better understanding

--- cloud/triggers/player.js
+++ cloud/triggers/player.js
@@ -1,8 +1,11 @@
 Parse.Cloud.afterSave("Player", function(request) {
+	//after you save the player you have to get the object out of the request
 	var player = request.object;
+	//if the player does not exist
 	if(!player.existed()) {
-		// It is newly created
+		//get the reference to the team he is playing for
 	  player.get("team").fetch().then(function(team) {
+		  //increment the number of players and save the object(team)
 		  team.increment("playerCount", 1);
 		  team.save();
 	  });
@@ -11,10 +14,14 @@ Parse.Cloud.afterSave("Player", function(request) {
 
 Parse.Cloud.afterDelete("Player", function(request) {
 	var player = request.object;
+	//if the player existed, but you delete it
 	if(player.existed()) {
 	  player.get("team").fetch().then(function(team) {
+		  //decrement the playerCount by 1
 		  team.increment("playerCount", -1);
 		  team.save();
 	  });
 	}
 });
+
+
